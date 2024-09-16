8dbf0ad5f3e0bc442ba44875b1cd55aa28f694cb
Alexander <165563699+sakaschekolda@users.noreply.github.com>
2024-09-16 16:59:57 +0400

    Update 5.go

--- 5.go
+++ 5.go
@@ -5,14 +5,14 @@ import "fmt"
 func main() {
 	var num1, num2 float64
 
-	fmt.Print("Введите первое число с плавающей запятой: ")
+	fmt.Print("Enter 1st float: ")
 	fmt.Scan(&num1)
-	fmt.Print("Введите второе число с плавающей запятой: ")
+	fmt.Print("Enter 2nd float: ")
 	fmt.Scan(&num2)
 
 	sum := num1 + num2
 	difference := num1 - num2
 
-	fmt.Printf("Сумма: %.2f + %.2f = %.2f\n", num1, num2, sum)
-	fmt.Printf("Разность: %.2f - %.2f = %.2f\n", num1, num2, difference)
+	fmt.Printf("Summary: %.2f + %.2f = %.2f\n", num1, num2, sum)
+	fmt.Printf("Difference: %.2f - %.2f = %.2f\n", num1, num2, difference)
 }
