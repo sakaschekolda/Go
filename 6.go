bf12cf4882e1ede2b95cf0bef5d796c1534d8fd5
Alexander <165563699+sakaschekolda@users.noreply.github.com>
2024-09-16 17:00:15 +0400

    Update 6.go

--- 6.go
+++ 6.go
@@ -5,14 +5,14 @@ import "fmt"
 func main() {
 	var num1, num2, num3 float64
 
-	fmt.Print("Введите первое число: ")
+	fmt.Print("Enter 1st number: ")
 	fmt.Scan(&num1)
-	fmt.Print("Введите второе число: ")
+	fmt.Print("Enter 2nd number: ")
 	fmt.Scan(&num2)
-	fmt.Print("Введите третье число: ")
+	fmt.Print("Enter 3rd number: ")
 	fmt.Scan(&num3)
 
 	average := (num1 + num2 + num3) / 3
 
-	fmt.Printf("Среднее значение: (%.2f + %.2f + %.2f) / 3 = %.2f\n", num1, num2, num3, average)
+	fmt.Printf("Average: (%.2f + %.2f + %.2f) / 3 = %.2f\n", num1, num2, num3, average)
 }
