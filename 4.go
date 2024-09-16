40ba601988ef5d29f990d591f74b6d6263eedd18
Alexander <165563699+sakaschekolda@users.noreply.github.com>
2024-09-16 16:59:17 +0400

    Update 4.go

--- 4.go
+++ 4.go
@@ -5,9 +5,9 @@ import "fmt"
 func main() {
 	var num1, num2 int
 
-	fmt.Print("Введите первое целое число: ")
+	fmt.Print("Enter 1st integer: ")
 	fmt.Scan(&num1)
-	fmt.Print("Введите второе целое число: ")
+	fmt.Print("Enter 2nd integer: ")
 	fmt.Scan(&num2)
 
 	sum := num1 + num2
@@ -15,8 +15,8 @@ func main() {
 	multiplication := num1 * num2
 	division := (num1) / (num2)
 
-	fmt.Printf("Сумма: %d + %d = %d\n", num1, num2, sum)
-	fmt.Printf("Разность: %d - %d = %d\n", num1, num2, difference)
-	fmt.Printf("Произведение: %d * %d = %d\n", num1, num2, multiplication)
-	fmt.Printf("Деление: %d / %d = %d.\n", num1, num2, division)
+	fmt.Printf("Summary: %d + %d = %d\n", num1, num2, sum)
+	fmt.Printf("Difference: %d - %d = %d\n", num1, num2, difference)
+	fmt.Printf("Multiplication: %d * %d = %d\n", num1, num2, multiplication)
+	fmt.Printf("Division: %d / %d = %d.\n", num1, num2, division)
 }
